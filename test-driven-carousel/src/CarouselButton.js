99aa0c3e73148c9cc53e159cd6cf5b21f45a742c
arsalon <arsalonamini2020@gmail.com>
2021-07-07 15:37:42 +0800

    TDD React Component - test for passing props

--- test-driven-carousel/src/CarouselButton.js
+++ test-driven-carousel/src/CarouselButton.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CarouselButton = () => <button />; //JSX treats lowercase as an instance of a DOM element | uppercase instance of component
+const CarouselButton = ({ children }) => <button>{children}</button>; //JSX treats lowercase as an instance of a DOM element | uppercase instance of component
+
+CarouselButton.propTypes = {
+  children: PropTypes.node.isRequired, 
+};
+
 
 export default CarouselButton;
--- test-driven-carousel/src/tests/CarouselButton.test.js
+++ test-driven-carousel/src/tests/CarouselButton.test.js
@@ -10,5 +10,10 @@ describe('CarouselButton', () => {
     it('renders a <button>', () => {
         const wrapper = shallow(<CarouselButton />);
         expect(wrapper.type()).toBe('button');
-    })
+    });
+    it('passes `children` through to the <button>', () => {
+        const text = 'Button text';
+        const wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
+        expect(wrapper.prop('children')).toBe(text);
+    });
 })
