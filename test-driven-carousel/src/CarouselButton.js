5164dffae913695d7158b46f22c33eafa1c6f275
arsalon <arsalonamini2020@gmail.com>
2021-07-07 15:25:30 +0800

    Testing React component using Jest

---
+++ test-driven-carousel/src/CarouselButton.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CarouselButton = () => <button />; //JSX treats lowercase as an instance of a DOM element | uppercase instance of component
+
+export default CarouselButton;
