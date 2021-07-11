b51d0b02b5a65621c2c48909f40b53e509cd68e7
arsalon <arsalonamini2020@gmail.com>
2021-07-11 15:19:37 +0800

    styled component init for img

--- test-driven-carousel/src/CarouselSlide.js
+++ test-driven-carousel/src/CarouselSlide.js
@@ -1,9 +1,19 @@
 import React from 'react'; 
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+//styled.img = fn that generates a component that renders <img> tag w/ given styles, using clas name selector 
+const Img = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 500px;
+`;
+
+
 
 const CarouselSlide = ({imgUrl, description, attribution, ...rest }) => (
     <figure {...rest}>
-        <img src={imgUrl} />
+        <Img src={imgUrl} />
         <figcaption>
            <strong>{description}</strong> {attribution}</figcaption>
     </figure>
