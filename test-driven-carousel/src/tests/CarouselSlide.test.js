3a05d9c5fba93c837e80f12d15207b7776bd8e2b
arsalon <arsalonamini2020@gmail.com>
2021-07-13 16:23:01 +0800

    overriding styled components

--- test-driven-carousel/src/tests/CarouselSlide.test.js
+++ test-driven-carousel/src/tests/CarouselSlide.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import CarouselSlide from '../CarouselSlide';
+import styled from 'styled-components';
 
 describe('CarouselSlide', () => {
     let wrapper;
@@ -75,6 +76,25 @@ describe('CarouselSlide', () => {
             mounted.setProps({ imgHeight: 'calc(100vh - 100px)' });
             expect(mounted).toHaveStyleRule('height', 'calc(100vh - 100px)');
         });
+
+        it('allows styles to be overridden', () => {
+            //extending styled components - pass an existing styled component to styled(), it'll return a new component with the stles from the original plus the new styles
+            const TestImg = styled(CarouselSlide.defaultProps.Img)`
+            width: auto;
+            height: auto;
+            object-fit: fill;
+            `;
+
+            mounted = mount(<CarouselSlide
+                Img={TestImg}
+                imgUlr={imgUrl}
+                description="this prop is required"
+            />);
+
+            expect(mounted.find(TestImg)).toHaveStyleRule('width', 'auto');
+            expect(mounted.find(TestImg)).toHaveStyleRule('height', 'auto');
+            expect(mounted.find(TestImg)).toHaveStyleRule('object-fit', 'fill');
+        })
     });
 
 })
